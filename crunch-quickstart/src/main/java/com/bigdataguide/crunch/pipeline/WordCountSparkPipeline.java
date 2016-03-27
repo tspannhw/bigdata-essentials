271d54d44780f3d393c34fc11377b1479ceeec04
vasu-d <vasu.doppalapudi@outlook.com>
2016-03-26 22:13:20 -0400

    crunch spark

--- crunch-quickstart/src/main/java/com/bigdataguide/crunch/pipeline/WordCountSparkPipeline.java
+++ crunch-quickstart/src/main/java/com/bigdataguide/crunch/pipeline/WordCountSparkPipeline.java
@@ -13,7 +13,6 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 
@@ -29,7 +28,7 @@ public class WordCountSparkPipeline extends Configured implements Tool {
     public int run(String[] args) throws Exception {
 
         if (args.length != 2) {
-            System.err.println("Usage: hadoop jar crunch-demo-1.0-SNAPSHOT-job.jar"
+            System.err.println("Usage: hadoop jar crunch-quickstart-1.0-SNAPSHOT.jar"
                                       + " [generic options] input output");
             System.err.println();
             GenericOptionsParser.printGenericCommandUsage(System.err);
@@ -40,28 +39,17 @@ public class WordCountSparkPipeline extends Configured implements Tool {
         String outputPath = args[1];
         
         JavaSparkContext context = new JavaSparkContext();
-        // Create an object to coordinate pipeline creation and execution.
+
         Pipeline pipeline = new SparkPipeline(context, "spark word count example");
 
-        // Reference a given text file as a collection of Strings.
         PCollection<String> lines = pipeline.readTextFile(inputPath);
 
-        // Define a function that splits each line in a PCollection of Strings into
-        // a PCollection made up of the individual words in the file.
-        // The second argument sets the serialization format.
         PCollection<String> words = lines.parallelDo(new Tokenizer(), Writables.strings());
 
-        // Take the collection of words and remove known stop words.
-        //PCollection<String> noStopWords = words.filter(new StopWordFilter());
-
-        // The count method applies a series of Crunch primitives and returns
-        // a map of the unique words in the input PCollection to their counts.
         PTable<String, Long> counts = words.count();
 
-        // Instruct the pipeline to write the resulting counts to a text file.
         pipeline.writeTextFile(counts, outputPath);
 
-        // Execute the pipeline as a MapReduce.
         PipelineResult result = pipeline.done();
         
         for (StageResult r : result.getStageResults()) {
